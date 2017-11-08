2eaa8722dbefc5511b70ff95b34a08a4eb7700b4
GB-dell <grzegorz.brodowski@outlook.com>
2017-11-08 22:03:05 +0100

    bezbłędne generowanie poprawnej tablicy obiektów

--- cmd_testy/Program.cs
+++ cmd_testy/Program.cs
@@ -11,16 +11,14 @@ namespace cmd_testy
         static void Main(string[] args)
         {
             Ankieta ankieta_test = new Ankieta();
-            ankieta_test.UtworzPytanie(5);
+            ankieta_test.UtworzPytanie(300);
         }
 
-        public class Ankieta
+        class Ankieta
         {
-            public string Test { get; set; }            
-
             public void UtworzPytanie(int m)
             {
-                Pytanie[] pytanie = new Pytanie[3];
+                Pytanie[] pytanie = new Pytanie[m];
                 //Pytanie[] pytanie = new Pytanie[m+1];
                 string p = "Pytanie";
                 Random rand = new Random();
@@ -34,37 +32,32 @@ namespace cmd_testy
 
                 for (int i = 0; i < m; i++)
                 {
-                    tresc_pytn[i] = p + "_" + (i+1).ToString() + ": " + "Tresc_pytania_" + i.ToString();
+                    tresc_pytn[i] = p + "_" + (i+1).ToString() + ": " + "Tresc_pytania_" + (i+1).ToString();
                 }
 
                 for (int i = 0; i < m; i++)
                 {
                     //Pytanie obiekty_pyt_nazwa = new Pytanie(1, "Która godzina?", 2, "11:32");
                     //Console.WriteLine(i.ToString());
-                    //pytanie[i] = new Pytanie(i, tresc_pytn[i], rand.Next(1, 5), DateTime.Today.ToString());
-                    pytanie[i] = new Pytanie("jakiś tekst");
+                    pytanie[i] = new Pytanie(i, tresc_pytn[i], rand.Next(1, 5), DateTime.Today.ToString());
+                    //pytanie[i] = new Pytanie("jakiś tekst");
                 }
 
-                //pytanie[0] = new Pytanie("jakiś tekst0");
-                //pytanie[1] = new Pytanie("jakiś tekst1");
-                //pytanie[2] = new Pytanie("jakiś tekst2");
-
-                //Console.WriteLine(" " + pytanie[0]);
-                //Console.WriteLine(" " + pytanie[1]);
-                //Console.WriteLine(" " + pytanie[2]);
-
-                //for (int i = 0; i < obiekty_pyt_nazwy.Length; i++)
-                //{
-                //    Console.WriteLine(" " + obiekty_pyt_nazwy[i]);
-                //    Console.WriteLine(" " + pytanie[i]);
-                //}
+                for (int i = 0; i < obiekty_pyt_nazwy.Length; i++)
+                {
+                    Console.WriteLine(" " + obiekty_pyt_nazwy[i]);
+                    Console.WriteLine(" " + pytanie[i].NumerPytania);
+                    Console.WriteLine(" " + pytanie[i].TrescPytania);
+                    Console.WriteLine(" " + pytanie[i].NumerOdpowiedzi);
+                    Console.WriteLine(" " + pytanie[i].TrescOdpowiedzi);
+                    Console.WriteLine("\n\n");
+                }
                 Console.ReadKey();
             }
         }
 
-        public class Pytanie
-        {
-
+        class Pytanie
+        { 
             public Pytanie(string TrescPytania)
             {
                 this.TrescPytania = TrescPytania;
@@ -74,14 +67,19 @@ namespace cmd_testy
             {
                 this.NumerPytania = NumerPytania;
                 this.TrescPytania = TrescPytania;
-                this.NumerPytania = NumerPytania;
+                this.NumerOdpowiedzi = NumerOdpowiedzi;
                 this.TrescOdpowiedzi = TrescOdpowiedzi;
             }
 
-            int NumerPytania { get; set; }
-            string TrescPytania { get; set; }
-            int NumerOdpowiedzi { get; set; }
-            string TrescOdpowiedzi { get; set; }
+            int numerPytania;
+            string trescPytania;
+            int numerOdpowiedzi;
+            string trescOdpowiedzi;
+
+            public int NumerPytania { get; set; }
+            public string TrescPytania { get; set; }
+            public int NumerOdpowiedzi { get; set; }
+            public string TrescOdpowiedzi { get; set; }
         }
     }
 }
